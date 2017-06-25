27fae3d620d1779fc6f156254896ee4e60c5a9b2
ovyan <ovyan13@ya.ru>
2017-06-25 15:20:28 +0300

    Update Extentions.swift

--- ForeheadDemo/Extentions.swift
+++ ForeheadDemo/Extentions.swift
@@ -1,11 +1,3 @@
-//
-//  Extentions.swift
-//  ForeheadDemo
-//
-//  Created by Евгений on 03.11.16.
-//  Copyright © 2016 Mike Ovyan. All rights reserved.
-//
-
 import UIKit
 
 let appKey = "iNDexCore"
